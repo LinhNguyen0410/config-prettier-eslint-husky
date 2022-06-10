1ce2c9eae69e3561f7618295a7da536f28548718
LinhNguyen0410 <linh.nttu18@gmail.com>
2022-06-10 23:53:52 +0700

    config prettier and eslint

---
+++ README.md
@@ -0,0 +1,16 @@
+# Package Installed
+
+prettier
+
+eslint
+
+ babel-eslint
+
+ eslint-config-react-app
+
+ eslint-config-prettier
+
+ eslint-plugin-prettier
+
+
+> https://prettier.io/playground/
---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import { useCallback } from 'react'
+import { useContext } from 'react'
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+	return (
+		<div className='App' style={{ backgroundColor: 'tomato' }}>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus magnam
+			odit maxime minima, deserunt ad, quae dolor a quis vitae, illo modi
+			repudiandae deleniti doloremque! Iure fugiat dolore commodi voluptas.
+			Fugiat ullam voluptatibus odit minus culpa hic ipsa exercitationem fugit
+			numquam earum, nulla a consequuntur modi molestias dolorum natus. Est
+			eligendi voluptatibus dolore ab facilis nihil laborum nostrum aliquid
+			adipisci sunt cumque labore natus consectetur vel tempora possimus, iure
+			eveniet nesciunt minus ratione ea assumenda quasi! Aspernatur iure at, id,
+			beatae quo assumenda accusantium perspiciatis asperiores quidem debitis,
+			cupiditate commodi. Vero similique assumenda, ratione dicta
+		</div>
+	)
+}
+
+export default App
